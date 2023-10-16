3990baa56a77ba7165f1f7504f5380fe43828fdc
Seth Martin <sgm13555@gmail.com>
2023-10-16 15:58:13 -0500

    Setup project 3

--- DOM_CrashCourse/DOM2/project02/project-2.js
+++ DOM_CrashCourse/DOM2/project02/project-2.js
@@ -1,1 +1,21 @@
 // Variables
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+openBtn.addEventListener("click", function () {
+  modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  modalContainer.style.display = "none";
+});
+
+// grab the event object (e)
+window.addEventListener("click", function (e) {
+  // e.target will return the element triggering the event.
+  // if target is the modalContainer, then apply style
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
