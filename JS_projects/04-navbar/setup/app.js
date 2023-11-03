a9394b2994a264d3a06ab08d7cf4946c5439dce0
Seth Martin <sgm13555@gmail.com>
2023-11-03 15:55:57 -0500

    Sidebar

--- JS_projects/04-navbar/setup/app.js
+++ JS_projects/04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  
+  // classList will return the list of assigned classes
+  // then we'll toggle the class 
+  links.classList.toggle("show-links");
+});
