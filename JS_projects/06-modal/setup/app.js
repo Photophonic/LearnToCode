7b34b0989bf961f29d3865bf6b16c54ea867c197
Seth Martin <sgm13555@gmail.com>
2023-11-17 15:53:45 -0600

    more projects

--- JS_projects/06-modal/setup/app.js
+++ JS_projects/06-modal/setup/app.js
@@ -1,4 +1,35 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+// const sidebarToggle = document.querySelector(".sidebar-toggle");
+
+// const sidebar = document.querySelector(".sidebar");
+// const closeBtn = document.querySelector(".close-btn");
+
+// sidebarToggle.addEventListener("click", function () {
+//   sidebar.classList.toggle("show-sidebar");
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
+
+const modalOverlay = document.querySelector(".modal-overlay");
+
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+// modalBtn.addEventListener("click", function () {
+//   modalOverlay.style.visibility = "visible";
+//   modalOverlay.style.zIndex = "20";
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   modalOverlay.style.visibility = "hidden";
+//   modalOverlay.style.zIndex = "-10";
+// });
+
+modalBtn.addEventListener("click", function () {
+  modalOverlay.classList.toggle("open-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  modalOverlay.classList.remove("open-modal");
+});
--- JS_projects/07-questions/setup/app.js
+++ JS_projects/07-questions/setup/app.js
@@ -1,2 +1,38 @@
 //using selectors inside the element
 // traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// // forEach is used to loop through an array of items
+// // passes a callback function for each element
+// // https://www.freecodecamp.org/news/javascript-foreach-how-to-loop-through-an-array-in-js/
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     // currentTarget identifes WHAT is being clicked
+//     //  traveres up the DOM up two spots.
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// using selectors inside the element vs. traversing the DOM
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  // create variable of buttons
+  const btn = question.querySelector(".question-btn");
+  // listen for click
+  btn.addEventListener("click", function () {
+    // loop over each question and compare if the current
+    // item matches the clicked, remove if not
+
+    questions.forEach(function (item) {
+      //console.log(item);
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    // add if match
+    question.classList.toggle("show-text");
+  });
+});
