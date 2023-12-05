a4db8c99644d2b5b5b061e9308a5933f14387562
Seth Martin <sgm13555@gmail.com>
2023-12-05 15:48:55 -0600

    Preloader project

--- JS_projects/09-video/setup/app.js
+++ JS_projects/09-video/setup/app.js
@@ -1,3 +1,27 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+const preload = document.querySelector(".preloader");
+
+// listen for load event on the window
+window.addEventListener("load", function () {
+  // once load is complete, add the hide class
+  preload.classList.add("hide-preloader");
+});
+
+// add event to btn
+btn.addEventListener("click", function () {
+  // if btn does not have slide class
+  if (!btn.classList.contains("slide")) {
+    // add slide (button will slide on screen)
+    btn.classList.add("slide");
+    // pause the video playing
+    video.pause();
+  } else {
+    // opposite of the above
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
