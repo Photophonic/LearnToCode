77617e1e5f52001dea4ef554d7478620e9e8f8e1
Seth Martin <sgm13555@gmail.com>
2023-12-26 15:59:22 -0600

    Count down setup

--- JS_projects/12-countdown-timer/setup/app.js
+++ JS_projects/12-countdown-timer/setup/app.js
@@ -21,3 +21,12 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+// use of cclass & element to target a specific group of h4
+const time = document.querySelectorAll(".deadline-format h4");
+
+// this will get a str of the current system time
+// YYYY, MM (0 index), DD, HH24, MM, SS
+let futureDate = new Date(2020, 4, 24, 17, 30, 0);
